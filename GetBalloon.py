57312a56b38da41c052ba58133a1d8fe906a973a
t8399955 <ido.bourvine.38.talpiot@gmail.com>
2017-12-25 14:32:34 +0200

    This is an updated version of the balloon detection. The "GetBalloon" holds a method that takes an image and returns a list of list of angles (x angle, y angle for each balloon detected). The "BalloonDetection" file has threads for 3 different cameras and a variable for each one of them that holds the angles needed for the balloons detected at all times.

--- GetBalloon.py
+++ GetBalloon.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 import math
-indexs = [0,7,10,12,13,14,15,16]
-for i in range(17):
-    index = str(i+1)
-    img = cv2.imread('im'+index+'.png')
+hor = 30.0
+ver = 25.0
+px = 640.0
+py = 480.0
+def getBall(img):
     ing = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([100, 140, 140])
+    lower_red = np.array([130, 100, 140])
     upper_red = np.array([200, 220, 255])
     mask = cv2.inRange(ing, lower_red, upper_red)
     res1 = cv2.bitwise_and(ing, ing, mask=mask)
@@ -15,7 +16,7 @@ for i in range(17):
     kernel3 = np.ones((6,6), np.uint8)
     close1 = cv2.morphologyEx(res1, cv2.MORPH_CLOSE, kernel1)
     open1 = cv2.morphologyEx(close1, cv2.MORPH_OPEN, kernel3)
-    gray = cv2.cvtColor(close1, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(open1, cv2.COLOR_HSV2BGR)
     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
     im1, contours, hier = cv2.findContours(gray, cv2.RETR_EXTERNAL,
                                       cv2.CHAIN_APPROX_NONE)
@@ -26,6 +27,8 @@ for i in range(17):
         im1, contours, hier = cv2.findContours(gray,cv2.RETR_TREE,
                                           cv2.CHAIN_APPROX_NONE)
     toDelete = []
+    bloons = []
+    i = 0
     for j in range(len(contours)):
         if(len(contours[j]) < 5):
             toDelete.append(contours[j])
@@ -37,12 +40,11 @@ for i in range(17):
         elif cv2.contourArea(contours[j]) < \
                                 0.5*0.25*ellipse[1][0]*ellipse[1][1]*math.pi:
             toDelete.append(contours[j])
-        elif cv2.contourArea(contours[j]) < 400:
+        elif cv2.contourArea(contours[j]) < 150:
             toDelete.append(contours[j])
         else:
-            green = (0,255,0)
-            cv2.ellipse(img, ellipse,(0,255,0), 3)
-    cv2.imshow(index, img)
-    cv2.waitKey(0)
-    #cv2.imshow(index,gray)
-    #cv2.waitKey(0)
+            i += 1
+            point = ellipse[0]
+            point = [(float(point[0]) - px/2)/px, -(float(point[1]) - py/2)/py]
+            bloons.append([point[0]*hor, point[1]*ver])
+    return bloons
