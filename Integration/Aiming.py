e198c87c28496e27c4032338543ce4f469e8568f
Idan <idanlevyehudi.38.talpiot@gmail.com>
2017-12-28 22:33:18 +0200

    Added commenting to all currently written code in integration

--- Integration/Aiming.py
+++ Integration/Aiming.py
@@ -1,18 +1,31 @@
+"""
+Serial communication for controlling the azimuth motor
+"""
 import serial
 import time
+
 
 class Aiming:
     def __init__(self, port, baudrate):
         self.ser = serial.Serial(port)
-        time.sleep(5)
-        self.ser.baudrate = baudrate
+        time.sleep(5)  # Need to wait for the port to open before addressing
+        #  the ser
+        self.ser.baudrate = baudrate  # Bandwidth of the serial comm
 
     def close(self):
         self.ser.close()
 
     def send(self, angle):
-        print ("about to send: " + str(angle % 256))
-        self.ser.write(str(angle % 256))
+        """
+        Sends an angle to the arduino
+        :param angle: angle to make the azimuth motor turn
+        :return: None
+        """
+
+        # Should be log
+        """print ("about to send: " + str(angle % 256))"""
+        self.ser.write(str(angle % 256))  # Code at arduino currently
+        # expects a single byte
 
 
 
