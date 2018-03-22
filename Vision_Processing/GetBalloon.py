5f78b535fbde3410f6797516e0614d0f6f052687
ido-bourvine <ido.bourvine.38.talpiot@gmail.com>
2018-03-22 20:56:28 +0200

    added balloon detection that does not rely on color

--- Vision_Processing/GetBalloon.py
+++ Vision_Processing/GetBalloon.py
@@ -115,9 +115,151 @@ def getColor(img):
 
 def didPop(imgBEFORE, imgAFTER):
     """returns true if red balloon popped, false otherwise"""
-    lowerBound = lower_red
-    upperBound = upper_red
-    min_size = MIN_SIZE
     color1 = getColor(imgBEFORE)
     color2 = getColor(imgAFTER)
+    color1 = cv2.cvtColor(color1, cv2.COLOR_BGR2GRAY)
+    color2 = cv2.cvtColor(color2, cv2.COLOR_BGR2GRAY)
+    numBefore = cv2.countNonZero(color1)
+    numAfter = cv2.countNonZero(color2)
+    if numBefore > numAfter + MIN_SIZE:
+        return 1
+    else:
+        return 0
+
+
+d = 30
+BAD1 = [5, 5, 5]
+d1 = 10
+BAD2 = [200, 200, 200]
+d2 = 56
+BAD3 = [5, 5, 250]
+d3 = 10
+BAD4 = [65, 15, 150]
+d4 = 25
+red_lower = [40, 50, 120]
+red_upper = [90, 100, 180]
+
+
+def canShoot1(circles):
+    """return if you can shoot or not"""
+    for circle in circles:
+        if (FIXED_PIX[0] - circle[0])**2 + (FIXED_PIX[1] - circle[1])**2 < \
+                circle[2]**2:
+            return 1
     return 0
+
+
+def getBalloon(img):
+    """returns a list, the first object is a boolean that says if you can
+    shoot or not, and the second one is a list of angles to each red balloon in
+    the image"""
+    bloons = getEnemies(img)[0]
+    can_shoot = canShoot1(bloons)
+    angles = []
+    for bloon in bloons:
+        bloon[0] = (bloon[0] - px/2) / px
+        bloon[1] = (py/2 - bloon[1]) / py
+        angles.append([bloon[0] * hor, bloon[1] * ver])
+    return [can_shoot, angles]
+
+
+def getEnemies(img):
+    """returns a list of enemy balloons and their sizes in image"""
+    red_bloons = []
+    red_sizes = []
+    bloons, sizes = getCircle(img)
+    for i in range(len(bloons)):
+        if isWhite(img, bloons[i]):
+            red_bloons.append(bloons[i])
+            red_sizes.append(sizes[i])
+    return [red_bloons, red_sizes]
+
+
+def getFriends(img):
+    """returns a list of friendly balloons and their sizes in image"""
+    friend_bloons = []
+    friend_sizes = []
+    bloons, sizes = getCircle(img)
+    for i in range(len(bloons)):
+        if not isWhite(img, bloons[i]):
+            friend_bloons.append(bloons[i])
+            friend_sizes.append(sizes[i])
+    return [friend_bloons, friend_sizes]
+
+
+def getCircle(img):
+    """returns a list of circles and their sizes in image"""
+    output = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 0.7, 40,
+                               param1=80, param2=15, minRadius=7,
+                               maxRadius=0)
+    bloons = []
+    sizes = []
+    if circles is not None:
+        circles = circles[0]  # syntax
+        for lst in circles:
+            x = lst[0]
+            y = lst[1]
+            r = lst[2]
+            if not isWhite(img, lst):
+                bloons.append(lst)
+                sizes.append(math.pi * r * r)
+                # cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+    return [bloons, sizes]
+
+
+def inBetween(ToCheck, BadArray, d):
+    """checks if ToCheck is in the range (badArray - d, badArray + d)"""
+    if (BadArray[0] - d < ToCheck[0] < BadArray[0] + d) and (
+                        BadArray[1] - d < ToCheck[1] < BadArray[
+                1] + d) and (BadArray[2] - d < ToCheck[2] <
+                                             BadArray[2] + d):
+        return True
+    else:
+        return False
+
+
+def manyConds(ToCheck, BadArrays, ds):
+    """checks if ToCheck is in one of the ranges around badArrays"""
+    for i in range(len(ds)):
+        if inBetween(ToCheck, BadArrays[i], ds[i]):
+            return True
+    return False
+
+
+def isClose(color, dist):
+    if abs(color[0] - color[1]) < dist and abs(color[1] - color[2]) < dist \
+            and \
+            abs(color[2] - color[0]) < dist:
+        return True
+    else:
+        return False
+
+
+def isRed(img, circle):
+    """checks if a circle average color is red-ish"""
+    circle = [int(X) for X in circle]
+    xc, yc, r = circle
+    cropImg = img[yc-r:yc+r, xc-r:xc+r]
+    average_color = cv2.mean(cropImg)
+    if red_lower[0] <= average_color[0] <= red_upper[0] and red_lower[1] <= \
+            average_color[1] <= red_upper[1] and red_lower[2] <= \
+            average_color[2] <= red_upper[2]:
+        return True
+    else:
+        return False
+
+
+def isWhite(img, circle):
+    """checks if a circles average color is white-ish"""
+    circle = [int(X) for X in circle]
+    xc, yc, r = circle
+    cropImg = img[yc-r:yc+r, xc-r:xc+r]
+    average_color = cv2.mean(cropImg)
+    if manyConds(average_color, [BAD1, BAD2, BAD3, BAD4], [d1, d2, d3,
+                                                               d4]) or \
+            isClose(average_color, d):
+        return True
+    else:
+        return False
