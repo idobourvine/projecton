6222f9095d197e247d539ed030ee132bc78bf03c
Idan <idanlevyehudi.38.talpiot@gmail.com>
2018-03-24 02:23:17 +0300

    Fixed confilcts in device map

--- CarCodes/Integration/Devices/DeviceMap.py
+++ CarCodes/Integration/Devices/DeviceMap.py
@@ -3,32 +3,22 @@ This class contains all of the physical devices of the car, to prevent
 attempting to intialize two instances of the same device
 """
 
-<<<<<<< HEAD
-import Vision_Processing.VisionData
-
 import Communication.Connection
-=======
 import Car_Camera.CarVisionData
 import Security_Cameras.SecurityVisionData
 import CarDrive
->>>>>>> mission_planning
 import TurretMotor
 
 
 class DeviceMap:
     def __init__(self):
-<<<<<<< HEAD
         self.connection = Communication.Connection.Connection(True)
 
-        self.vision_data = \
-            Vision_Processing.VisionData.VisionData(self.connection)
-=======
         self.car_vision_data = \
             Car_Camera.CarVisionData.CarVisionData()
 
         self.security_vision_data = \
             Security_Cameras.SecurityVisionData.SecurityVisionData()
->>>>>>> mission_planning
 
         # serial comm with arduino that controls azimuth motor
         # self.azimuth_motor = TurretMotor.TurretMotor('COM5')  # PC Port
