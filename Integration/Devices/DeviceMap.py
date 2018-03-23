83e43c3f286487fc72f3ebcad8d3f9cffb10adc3
Idan <idanlevyehudi.38.talpiot@gmail.com>
2018-03-23 14:13:16 +0300

    Testing connection with sending hi

--- Integration/Devices/DeviceMap.py
+++ Integration/Devices/DeviceMap.py
@@ -4,13 +4,17 @@ attempting to intialize two instances of the same device
 """
 
 import Vision_Processing.VisionData
+
+import Communication.Connection
 import TurretMotor
 
 
 class DeviceMap:
     def __init__(self):
+        self.connection = Communication.Connection.Connection(True)
+
         self.vision_data = \
-            Vision_Processing.VisionData.VisionData()
+            Vision_Processing.VisionData.VisionData(self.connection)
 
         self.azimuth_motor = TurretMotor.TurretMotor('COM5')
         # serial comm with arduino that controls azimuth motor
