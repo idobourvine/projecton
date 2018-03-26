5b5880789b6eb3a3ab2d86958117f557b69c5fc8
Idan <idanlevyehudi.38.talpiot@gmail.com>
2018-03-26 17:44:14 +0300

    Added delay

--- CarCodes/Integration/Car_Camera/CarVisionData.py
+++ CarCodes/Integration/Car_Camera/CarVisionData.py
@@ -59,7 +59,7 @@ class CarVisionData:
         return copy.deepcopy(self.room_bloons)
 
     def get_car_position(self):
-        return (400, 200, 30)
+        return (360, 180, 30)
 
     def get_continue_mission(self):
         return copy.deepcopy(self.continue_mission)
