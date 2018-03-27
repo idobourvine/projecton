36b074525b8c16fca0c71672167932c308fc7c85
Idan <idanlevyehudi.38.talpiot@gmail.com>
2018-03-27 09:01:13 +0300

    now without the bloons

--- CarCodes/Integration/Missions/Planning/MissionPlanner.py
+++ CarCodes/Integration/Missions/Planning/MissionPlanner.py
@@ -102,14 +102,14 @@ class MissionPlanner:
                 print("Running Tests")
                 print()
 
-                if len(room_bloons) == 0:
-                    return None
-                if not isinstance(room_bloons[0], collections.Iterable):
-                    room_bloons = [room_bloons]
+                # if len(room_bloons) == 0:
+                #     return None
+                # if not isinstance(room_bloons[0], collections.Iterable):
+                #     room_bloons = [room_bloons]
 
                 # If the test bloons are further away than the system
-                rel_bloons = [bloon for bloon in room_bloons if bloon[0] >
-                              curr_position[0]]
+                # rel_bloons = [bloon for bloon in room_bloons if bloon[0] >
+                #               curr_position[0]]
 
                 # mis = Missions.Turret.ClearStandpoint.ClearStandpoint(
                 #     self.device_map, rel_bloons, curr_position, curr_ori)
